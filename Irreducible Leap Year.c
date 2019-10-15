8f21348a20181ab93aad582bee6c8136ef0561a3
aha_meow <41865765+ahameow@users.noreply.github.com>
2019-10-15 19:35:15 +0800

    Update Irreducible Leap Year.c

--- Irreducible Leap Year.c
+++ Irreducible Leap Year.c
@@ -7,7 +7,7 @@ int main()
     printf("Enter the year:");
     scanf("%d",&y);
 
-    if (y%4==0)           //普通闰年||世纪闰年||世纪闰年的前一个闰年的后四年即为下一个闰年
+    if (y%4==0) 
         y++;
 
         while(y%100==0&&y%400!=0||y%4!=0)
